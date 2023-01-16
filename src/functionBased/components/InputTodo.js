49c87547543cab2ba994125e50055ca7f4211579
willy836 <wilsonkimiri836@gmail.com>
2023-01-16 18:29:52 +0300

    Complete todo app using classes and refactor to use functional components

---
+++ src/functionBased/components/InputTodo.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+const InputTodo = (props) => {
+  const [inputText, setInputText] = useState({
+    title: "",
+  });
+
+  const onChange = (e) => {
+    setInputText({
+      ...inputText,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputText.title.trim()) {
+      props.addTodoProps(inputText.title);
+      setInputText({
+        title: "",
+      });
+    } else {
+      alert("Please enter an item");
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className="form-container">
+        <input
+          type="text"
+          className="input-text"
+          placeholder="Add Todo..."
+          value={inputText.title}
+          name="title"
+          onChange={onChange}
+        />
+        <button className="input-submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export { InputTodo };
